32e9575c220c554fd007a02aa97297497161f61e
Argyaboy20 <smpn21985@gmail.com>
2025-07-02 12:56:14 +0700

    memilih tailwind yang lebih kompatibel

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
